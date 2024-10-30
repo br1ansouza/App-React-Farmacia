e3ad8766b9aedb4d02c4c73bb4b48a4c94a01dd4
Brian de Souza <briandesouza1597@gmail.com>
2024-10-30 10:01:34 -0300

    Corrigindo tela de movimentações, a tela deve atualizar automaticamente após a adição de nova movimentação

--- src/Pages/Movements/ListMovements.jsx
+++ src/Pages/Movements/ListMovements.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList, Alert } from 'react-native';
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons'; // verificar se vou utilizar ainda
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Picker } from '@react-native-picker/picker';
 import { FontAwesome } from '@expo/vector-icons';
@@ -11,8 +11,9 @@ export default function ListMovements() {
     const [movements, setMovements] = useState([]);
     const [userProfile, setUserProfile] = useState('');
     const [statusMap, setStatusMap] = useState({}); // mapeia os status das movimentações
-    const [activeTab, setActiveTab] = useState('ativas'); // controla a aba ativa (ativas/canceladas)
+    const [activeTab, setActiveTab] = useState('ativas'); // aba ativa (ativas/canceladas)
     const navigation = useNavigation();
+    const isFocused = useIsFocused();
 
     // função para buscar o perfil do usuário
     const fetchUserProfile = async () => {
@@ -86,230 +87,232 @@ export default function ListMovements() {
         );
     };
 */}
-   // separar movimentações ativas e finalizadas/canceladas
-   const activeMovements = movements.filter(movement => statusMap[movement.id] !== 'cancelada' && statusMap[movement.id] !== 'finalizada');
-   const canceledMovements = movements.filter(movement => statusMap[movement.id] === 'cancelada' || statusMap[movement.id] === 'finalizada');
+    // separar movimentações ativas e finalizadas/canceladas
+    const activeMovements = movements.filter(movement => statusMap[movement.id] !== 'cancelada' && statusMap[movement.id] !== 'finalizada');
+    const canceledMovements = movements.filter(movement => statusMap[movement.id] === 'cancelada' || statusMap[movement.id] === 'finalizada');
 
-   // carregar a lista de movimentações e o perfil do usuário ao montar o componente
-   useEffect(() => {
-       fetchMovements();
-       fetchUserProfile();
-   }, []);
+    // carregar a lista de movimentações e o perfil do usuário ao montar o componente
+    useEffect(() => {
+        if (isFocused) { // Recarrega os dados apenas quando a tela está em foco
+            fetchMovements();
+        }
+        fetchUserProfile();
+    }, [isFocused]);
 
-   // renderiza movimentação/item da lista
-   const renderMovementItem = ({ item }) => {
-       const selectedStatus = statusMap[item.id] || item.status;
+    // renderiza movimentação/item da lista
+    const renderMovementItem = ({ item }) => {
+        const selectedStatus = statusMap[item.id] || item.status;
 
-       return (
-           <View style={styles.card}>
-               <View style={styles.cardContent}>
-                   {/* botão para navegar para detalhes da movimentação, visível apenas para usuários que não são "filial" */}
-                   {userProfile !== 'filial' && (
-                       <TouchableOpacity
-                           style={styles.iconButton}
-                           onPress={() => navigation.navigate('DriverMovements', { movementId: item.id })}
-                       >
-                           <MaterialIcons name="play-circle-outline" size={39} color="#288737" backgroundColor="#fff" />
-                       </TouchableOpacity>
-                   )}
+        return (
+            <View style={styles.card}>
+                <View style={styles.cardContent}>
+                    {/* botão para navegar para detalhes da movimentação, visível apenas para usuários que não são "filial" */}
+                    {userProfile !== 'filial' && (
+                        <TouchableOpacity
+                            style={styles.iconButton}
+                            onPress={() => navigation.navigate('DriverMovements', { movementId: item.id })}
+                        >
+                            <MaterialIcons name="play-circle-outline" size={39} color="#288737" backgroundColor="#fff" />
+                        </TouchableOpacity>
+                    )}
 
-                   <Text style={styles.movementDetail}><Text style={styles.bold}>Origem:</Text> {item.origem?.nome || 'Desconhecido'}</Text>
-                   <Text style={styles.movementDetail}><Text style={styles.bold}>Destino:</Text> {item.destino?.nome || 'Desconhecido'}</Text>
-                   <Text style={styles.movementDetail}><Text style={styles.bold}>Produto:</Text> {item.produto?.nome || 'Desconhecido'}</Text>
-                   <Text style={styles.movementDetail}><Text style={styles.bold}>Quantidade:</Text> {item.quantidade || 'N/A'}</Text>
-                   <Text style={styles.movementDetail}><Text style={styles.bold}>Observações:</Text> {item.observacoes || 'Nenhuma'}</Text>
+                    <Text style={styles.movementDetail}><Text style={styles.bold}>Origem:</Text> {item.origem?.nome || 'Desconhecido'}</Text>
+                    <Text style={styles.movementDetail}><Text style={styles.bold}>Destino:</Text> {item.destino?.nome || 'Desconhecido'}</Text>
+                    <Text style={styles.movementDetail}><Text style={styles.bold}>Produto:</Text> {item.produto?.nome || 'Desconhecido'}</Text>
+                    <Text style={styles.movementDetail}><Text style={styles.bold}>Quantidade:</Text> {item.quantidade || 'N/A'}</Text>
+                    <Text style={styles.movementDetail}><Text style={styles.bold}>Observações:</Text> {item.observacoes || 'Nenhuma'}</Text>
 
-                   <Text style={styles.movementDetail}><Text style={styles.bold}>Status atual:</Text> {selectedStatus}</Text>
+                    <Text style={styles.movementDetail}><Text style={styles.bold}>Status atual:</Text> {selectedStatus}</Text>
 
-                   {/* Picker de status visível apenas para usuários que não são "filial" */}
-                   {userProfile !== 'filial' && (
-                       <View style={styles.pickerContainer}>
-                           <Picker
-                               selectedValue={selectedStatus}
-                               onValueChange={(itemValue) => updateMovementStatus(item.id, itemValue)}
-                               style={styles.picker}
-                           >
-                               <Picker.Item label="Selecionar status " value={selectedStatus} />
-                               <Picker.Item label="Movimentação iniciada ▶️" value="iniciada" />
-                               <Picker.Item label="Movimentação finalizada ✅" value="finalizada" />
-                               <Picker.Item label="Movimentação cancelada ❌" value="cancelada" />
-                           </Picker>
-                       </View>
-                   )}
+                    {/* picker de status visível apenas para usuários que não são do tipo "filial" */}
+                    {userProfile !== 'filial' && (
+                        <View style={styles.pickerContainer}>
+                            <Picker
+                                selectedValue={selectedStatus}
+                                onValueChange={(itemValue) => updateMovementStatus(item.id, itemValue)}
+                                style={styles.picker}
+                            >
+                                <Picker.Item label="Selecionar status " value={selectedStatus} />
+                                <Picker.Item label="Movimentação iniciada ▶️" value="iniciada" />
+                                <Picker.Item label="Movimentação finalizada ✅" value="finalizada" />
+                                <Picker.Item label="Movimentação cancelada ❌" value="cancelada" />
+                            </Picker>
+                        </View>
+                    )}
 
-                   {/* botão para deletar a movimentação */}
-                   <TouchableOpacity
-                       style={styles.deleteButton}
-                       onPress={() => deleteMovement(item.id)}
-                   >
-                       <Text style={styles.deleteButtonText}>Deletar</Text>
-                   </TouchableOpacity>
-               </View>
-           </View>
-       );
-   };
+                    {/* botão para deletar a movimentação */}
+                    <TouchableOpacity
+                        style={styles.deleteButton}
+                        onPress={() => deleteMovement(item.id)}
+                    >
+                        <Text style={styles.deleteButtonText}>Deletar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    };
 
-   return (
-       <View style={styles.container}>
-           <Text style={styles.headerText}>Movimentações</Text>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.headerText}>Movimentações</Text>
 
-           {/* mostrar o botão "Adicionar movimentação" apenas para perfis que não sejam do tipo "motorista" */}
-           {userProfile !== 'motorista' && (
-               <TouchableOpacity
-                   style={styles.newMovementButton}
-                   onPress={() => navigation.navigate('RegisterMovement')}
-               >
-                   <Text style={styles.newMovementButtonText}>Adicionar movimentação</Text>
-               </TouchableOpacity>
-           )}
+            {/* mostrar o botão "Adicionar movimentação" apenas para perfis que não sejam do tipo "motorista" */}
+            {userProfile !== 'motorista' && (
+                <TouchableOpacity
+                    style={styles.newMovementButton}
+                    onPress={() => navigation.navigate('RegisterMovement')}
+                >
+                    <Text style={styles.newMovementButtonText}>Adicionar movimentação</Text>
+                </TouchableOpacity>
+            )}
 
-           {/* list movimentações - aba ativa */}
-           <FlatList
-               data={activeTab === 'ativas' ? activeMovements : canceledMovements}
-               keyExtractor={(item) => item.id.toString()}
-               renderItem={renderMovementItem}
-               contentContainerStyle={styles.list}
-               ListEmptyComponent={
-                   <Text style={styles.noMovementsText}>
-                       {activeTab === 'ativas' ? 'Nenhuma movimentação ativa encontrada.' : 'Nenhuma movimentação cancelada/finalizada.'}
-                   </Text>
-               }
-           />
+            {/* list movimentações - aba ativa */}
+            <FlatList
+                data={activeTab === 'ativas' ? activeMovements : canceledMovements}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={renderMovementItem}
+                contentContainerStyle={styles.list}
+                ListEmptyComponent={
+                    <Text style={styles.noMovementsText}>
+                        {activeTab === 'ativas' ? 'Nenhuma movimentação ativa encontrada.' : 'Nenhuma movimentação cancelada/finalizada.'}
+                    </Text>
+                }
+            />
 
-           {/* footer fixo com abas para alternar entre ativas/canceladas */}
-           <View style={styles.footer}>
-               <TouchableOpacity
-                   style={[styles.footerButton, activeTab === 'ativas' && styles.activeTab]}
-                   onPress={() => setActiveTab('ativas')}
-               >
-                   <FontAwesome name="list-alt" size={24} color={activeTab === 'ativas' ? '#6200EE' : '#fff'} />
-                   <Text style={styles.footerText}>Ativas</Text>
-               </TouchableOpacity>
+            {/* footer fixo com abas para alternar entre ativas/canceladas */}
+            <View style={styles.footer}>
+                <TouchableOpacity
+                    style={[styles.footerButton, activeTab === 'ativas' && styles.activeTab]}
+                    onPress={() => setActiveTab('ativas')}
+                >
+                    <FontAwesome name="list-alt" size={24} color={activeTab === 'ativas' ? '#6200EE' : '#fff'} />
+                    <Text style={styles.footerText}>Ativas</Text>
+                </TouchableOpacity>
 
-               <TouchableOpacity
-                   style={[styles.footerButton, activeTab === 'canceladas' && styles.activeTab]}
-                   onPress={() => setActiveTab('canceladas')}
-               >
-                   <FontAwesome name="times-circle" size={24} color={activeTab === 'canceladas' ? '#6200EE' : '#fff'} />
-                   <Text style={styles.footerText}>Finalizadas</Text>
-               </TouchableOpacity>
-           </View>
-       </View>
-   );
+                <TouchableOpacity
+                    style={[styles.footerButton, activeTab === 'canceladas' && styles.activeTab]}
+                    onPress={() => setActiveTab('canceladas')}
+                >
+                    <FontAwesome name="times-circle" size={24} color={activeTab === 'canceladas' ? '#6200EE' : '#fff'} />
+                    <Text style={styles.footerText}>Finalizadas</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-   container: {
-       flex: 1,
-       backgroundColor: '#121212',
-   },
-   headerText: {
-       fontSize: 20,
-       fontWeight: 'bold',
-       color: '#fff',
-       marginVertical: 20,
-       paddingHorizontal: 20,
-   },
-   newMovementButton: {
-       backgroundColor: '#6200EE',
-       paddingVertical: 10,
-       paddingHorizontal: 20,
-       borderRadius: 8,
-       marginBottom: 20,
-       alignSelf: 'center',
-   },
-   newMovementButtonText: {
-       color: '#fff',
-       fontWeight: 'bold',
-       fontSize: 16,
-       textAlign: 'center',
-   },
-   list: {
-       paddingBottom: 100, // espaço extra para evitar sobreposição do footer
-       paddingHorizontal: 20,
-   },
-   card: {
-       backgroundColor: '#fff',
-       borderRadius: 12,
-       padding: 20,
-       marginBottom: 15,
-       borderWidth: 1,
-       borderColor: '#E0E0E0',
-       shadowColor: '#fff',
-       shadowOffset: { width: 0, height: 2 },
-       shadowOpacity: 0.1,
-       shadowRadius: 4,
-       elevation: 3,
-   },
-   cardContent: {
-       flexDirection: 'column',
-   },
-   movementDetail: {
-       fontSize: 16,
-       color: '#333',
-       marginBottom: 5,
-   },
-   bold: {
-       fontWeight: 'bold',
-       color: '#000',
-   },
-   pickerContainer: {
-       borderColor: '#6200EE',
-       borderWidth: 1,
-       borderRadius: 8,
-       backgroundColor: '#FFFFFF',
-       marginTop: 10,
-       width: '100%',
-   },
-   picker: {
-       height: 50,
-       color: '#333',
-   },
-   iconButton: {
-       position: 'absolute',
-       right: 10,
-       top: 10,
-       backgroundColor: 'transparent',
-   },
-   noMovementsText: {
-       color: '#fff',
-       fontSize: 18,
-       textAlign: 'center',
-       marginTop: 20,
-   },
-   footer: {
-       flexDirection: 'row',
-       justifyContent: 'space-around',
-       backgroundColor: '#101010',
-       paddingVertical: 10,
-       borderTopWidth: 1,
-       borderTopColor: '#454545',
-       position: 'absolute',
-       bottom: 0,
-       left: 0,
-       right: 0,
-   },
-   footerButton: {
-       alignItems: 'center',
-   },
-   footerText: {
-       color: '#fff',
-       fontSize: 14,
-   },
-   activeTab: {
-       borderBottomWidth: 2,
-       borderBottomColor: '#6200EE',
-   },
-   deleteButton: {
-       backgroundColor: '#FF6347',
-       paddingVertical: 8,
-       paddingHorizontal: 10,
-       borderRadius: 8,
-       alignSelf: 'flex-end',
-       marginTop: 10,
-   },
-   deleteButtonText: {
-       color: '#fff',
-       fontWeight: 'bold',
-   },
+    container: {
+        flex: 1,
+        backgroundColor: '#121212',
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginVertical: 20,
+        paddingHorizontal: 20,
+    },
+    newMovementButton: {
+        backgroundColor: '#6200EE',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 8,
+        marginBottom: 20,
+        alignSelf: 'center',
+    },
+    newMovementButtonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    list: {
+        paddingBottom: 100, // espaço extra para evitar sobreposição do footer
+        paddingHorizontal: 20,
+    },
+    card: {
+        backgroundColor: '#fff',
+        borderRadius: 12,
+        padding: 20,
+        marginBottom: 15,
+        borderWidth: 1,
+        borderColor: '#E0E0E0',
+        shadowColor: '#fff',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    cardContent: {
+        flexDirection: 'column',
+    },
+    movementDetail: {
+        fontSize: 16,
+        color: '#333',
+        marginBottom: 5,
+    },
+    bold: {
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    pickerContainer: {
+        borderColor: '#6200EE',
+        borderWidth: 1,
+        borderRadius: 8,
+        backgroundColor: '#FFFFFF',
+        marginTop: 10,
+        width: '100%',
+    },
+    picker: {
+        height: 50,
+        color: '#333',
+    },
+    iconButton: {
+        position: 'absolute',
+        right: 10,
+        top: 10,
+        backgroundColor: 'transparent',
+    },
+    noMovementsText: {
+        color: '#fff',
+        fontSize: 18,
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        backgroundColor: '#101010',
+        paddingVertical: 10,
+        borderTopWidth: 1,
+        borderTopColor: '#454545',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+    footerButton: {
+        alignItems: 'center',
+    },
+    footerText: {
+        color: '#fff',
+        fontSize: 14,
+    },
+    activeTab: {
+        borderBottomWidth: 2,
+        borderBottomColor: '#6200EE',
+    },
+    deleteButton: {
+        backgroundColor: '#FF6347',
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        borderRadius: 8,
+        alignSelf: 'flex-end',
+        marginTop: 10,
+    },
+    deleteButtonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
 });
